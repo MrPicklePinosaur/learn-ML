8efe187cd111e812dd0e50c858b7b0cbeb80a834
Daniel Liu <mr.picklepinosaur@gmail.com>
2019-08-02 18:32:02 -0400

    decision tree construction;

--- sklearn/DTree.py
+++ sklearn/DTree.py
@@ -1,33 +1,63 @@
 import numpy as np
+import sys
 from pprint import *
 
 class DTree:
 
+	def __init__(self):
+		self.root = None
+
 	def fit(self,dataset): #this is where we build the tree
-		pass
+		
+		#Find the best question to ask at this node
+		q = self.find_best_question(dataset)
+
+		#sorta inefficient but we are going to have to partition data again
+		part = q.partition(dataset)
+		true_set = part["true"]
+		false_set = part["false"]
+
+		#TODO: DONT DO len(true_set), instead, check for unique features
+		if len(true_set) > 0: #if we havent reached a leaf
+			q.true_node = self.fit(true_set)
+		if len(false_set) > 0:
+			q.false_node = self.fit(false_set)
+
+		return q
 
 	def predict(self):
 		pass
 
-	def ask_questions(self,dataset):
+	def find_best_question(self,dataset):
+		if dataset == None or len(dataset) == 0: 
+			return None
+
 		#generate questions =-=-=-=-=
 		#first find all unique features (this breaks if data is empty)
 		labels = DTree.remove_labels(dataset) #truncate dataset so we only look at features
 		unq_labels = DTree.find_unique_features(labels)
+		
+		cur_gini = DTree.gini(dataset) #Find current impurity
+		best_gini = sys.maxsize
+		best_question = None
 
 		#now iterate through the labels and find out what the best question to ask is
 		for label in unq_labels:
 			q = Question(unq_labels[label],label)
-			print("Question is",label)
 
 			#Now partition the data and find the gini impurity and info gain
 			part = q.partition(dataset)
 			true_set = part["true"]
 			false_set = part["false"]
-			print(true_set)
-			print(false_set)
 
-		
+			#Deterime a weighted gini value for this question
+			new_gini = DTree.gini(true_set)*(len(true_set)/len(dataset)) + DTree.gini(false_set)*(len(false_set)/len(dataset))
+
+			if new_gini < best_gini: #NOTE: this might be wrong, try using info gain instead
+				best_gini = new_gini
+				best_question = q
+
+		return best_question
 
 	#helper methods
 	@staticmethod
@@ -47,11 +77,37 @@ class DTree:
 			new_dataset.append(datapoint[:len(datapoint)-1])
 		return new_dataset
 
+	@staticmethod
+	def get_occurences(dataset): #returns a dict with the amount of occurences each element has
+		occurs = {}
+		for row in dataset:
+			label = row[-1] #the label is always the last item in row 
+			if label in occurs:
+				occurs[label] += 1
+			else:
+				occurs[label] = 1
+		return occurs
+
+	@staticmethod
+	def gini(dataset): #input needs to be a 1D array of labels
+		occurs = DTree.get_occurences(dataset)
+		impurity = 1
+		for label in occurs:
+			prob = occurs[label] / len(dataset)
+			impurity -= prob**2
+		return impurity
+
 class Question:
 
 	def __init__(self,column,feature):
 		self.column = column #the column number for the feature
 		self.feature = feature #the specific feature
+
+		self.true_node = None
+		self.false_node = None
+
+	def __repr__(self):
+		return self.feature
 
 	def match(self,example):
 		if Question.is_numeric(self.feature): #if the feature is numeric, ask inequality questions
@@ -82,6 +138,7 @@ training_data = [
 ]
 
 dtree = DTree()
-dtree.ask_questions(training_data)
+dtree.fit(training_data)
 
+#print(DTree.get_occurences(training_data))
 #pprint(np.delete(training_data,len(training_data[0])-1,axis=1))
